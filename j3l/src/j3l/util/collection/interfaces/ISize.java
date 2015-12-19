67fe73e21f61b42ee03edc75a9aba66059fefa23
Johannes B. Latzel <johannes.latzel@web.de>
2015-12-19 17:05:45 +0100

    see Changelog

---
+++ j3l/src/j3l/util/collection/interfaces/ISize.java
@@ -0,0 +1,21 @@
+package j3l.util.collection.interfaces;
+
+/**
+ * <p></p>
+ * 
+ * @since JDK 1.8
+ * @version 2015.12.14_0
+ * @author Johannes B. Latzel
+ */
+public interface ISize {
+	
+	
+	/**
+	 * <p></p>
+	 *
+	 * @param
+	 * @return
+	 */
+	long getSize();
+	
+}
