f98bda007ab2fce78712719778ea1ccbd6abdad2
Johannes B. Latzel <johannes.latzel@web.de>
2016-07-10 15:59:32 +0200

    merged "j3l.util.close.IStateClosure" into "j3l.util.close.IClose"

--- src/j3l/util/close/IClose.java
+++ src/j3l/util/close/IClose.java
@@ -5,10 +5,10 @@ package j3l.util.close;
  * <p></p>
  * 
  * @since JDK 1.8
- * @version 2015.12.12_0
+ * @version 2016.07.10_0
  * @author Johannes B. Latzel
  */
-public interface IClose<T extends Exception> extends AutoCloseable, IStateClosure {
+public interface IClose<T extends Exception> extends AutoCloseable {
 	
 	
 	/**
@@ -28,5 +28,69 @@ public interface IClose<T extends Exception> extends AutoCloseable, IStateClosur
 	 * @return
 	 */
 	@Override void close() throws T;
+	
+	
+	/**
+	 * <p></p>
+	 *
+	 * @param
+	 * @return
+	 */
+	ClosureState getClosureState();
+	
+	
+	/**
+	 * <p></p>
+	 *
+	 * @param
+	 * @return
+	 */
+	public default boolean isInOpening() {
+		return getClosureState().compareTo(ClosureState.InOpening) == 0;
+	}
+	
+	
+	/**
+	 * <p></p>
+	 *
+	 * @param
+	 * @return
+	 */
+	public default boolean isOpen() {
+		return getClosureState().compareTo(ClosureState.Open) == 0;
+	}
+	
+	
+	/**
+	 * <p></p>
+	 *
+	 * @param
+	 * @return
+	 */
+	public default boolean isInClosure() {
+		return getClosureState().compareTo(ClosureState.InClosure) == 0;
+	}
+	
+	
+	/**
+	 * <p></p>
+	 *
+	 * @param
+	 * @return
+	 */
+	public default boolean isClosed() {
+		return getClosureState().compareTo(ClosureState.Closed) == 0;
+	}
+	
+	
+	/**
+	 * <p></p>
+	 *
+	 * @param
+	 * @return
+	 */
+	public default boolean hasBeenOpened() {
+		return getClosureState().compareTo(ClosureState.Open) >= 0;
+	}
 	
 }
