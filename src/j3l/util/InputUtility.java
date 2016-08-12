ed56baa058525c8d67d725af2e9880516464257c
Johannes B. Latzel <johannes.latzel@web.de>
2016-08-12 17:08:53 +0200

    added support for java.nio.ReadableByteChannel

--- src/j3l/util/InputUtility.java
+++ src/j3l/util/InputUtility.java
@@ -3,19 +3,24 @@ package j3l.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
 
 /**
- * <p>utility-class for {@link java.io.Reader reader} and {@link java.io.InputStream input streams}</p>
+ * <p>
+ * 		utility-class for {@link java.io.Reader reader}, {@link java.io.InputStream streams}, and
+ *		{@link java.nio.ReadableByteChannel channel}
+ * </p>
  * 
  * @since JDK 1.8
- * @version 2016.06.10_0
+ * @version 2016.08.12_0
  * @author Johannes B. Latzel
  */
 public final class InputUtility {
 
 	
 	/**
-	 * <p>reads characters into the buffer as long as the buffer is completely filled</p>
+	 * <p>reads characters into the buffer as long as the buffer is not completely filled</p>
 	 * <p>notice: 
 	 * 	<li>proceeds as long as the buffer is not completely filled or an exception is thrown</li>
 	 * 	<li>returns the same object which is passed by the parameter buffer</li>
@@ -42,7 +47,7 @@ public final class InputUtility {
 	
 	
 	/**
-	 * <p>reads bytes into the buffer as long as the buffer is completely filled</p>
+	 * <p>reads bytes into the buffer as long as the buffer is not completely filled</p>
 	 * <p>notice: 
 	 * 	<li>proceeds as long as the buffer is not completely filled or an exception is thrown</li>
 	 * 	<li>returns the same object which is passed by the parameter buffer</li>
@@ -59,6 +64,33 @@ public final class InputUtility {
 		int current_read_in_bytes;
 		while( actually_read < buffer_size ) {
 			current_read_in_bytes = input_stream.read(buffer, actually_read, (buffer_size - actually_read));
+			if( current_read_in_bytes < 0 ) {
+				throw new IOException("Reached EOF while reading!");
+			}
+			actually_read += current_read_in_bytes;
+		}
+		return buffer;
+	}
+	
+	
+	/**
+	 * <p>reads bytes into the buffer as long as the buffer is not completely filled</p>
+	 * <p>notice: 
+	 * 	<li>proceeds as long as the buffer is not completely filled or an exception is thrown</li>
+	 * 	<li>returns the same object which is passed by the parameter buffer</li>
+	 * </p>
+	 *
+	 * @param input_stream any input stream
+	 * @param buffer the buffer in which the bytes are read into
+	 * @return the buffer
+	 * @throws IOException
+	 */
+	public static ByteBuffer readComplete(ReadableByteChannel channel, ByteBuffer buffer) throws IOException {
+		int actually_read = 0;
+		int buffer_size = buffer.capacity();
+		int current_read_in_bytes;
+		while( actually_read < buffer_size ) {
+			current_read_in_bytes = channel.read(buffer);
 			if( current_read_in_bytes < 0 ) {
 				throw new IOException("Reached EOF while reading!");
 			}
